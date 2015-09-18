2fe0c99a5a99a1051db55af319de4570fdcdfa15
hduser <github@newnham.com>
2015-09-18 15:45:39 +0100

    Reducer1Test - Renamed ReducerTest to Reducer1Test. Test works exclusively on Reduce1, which is a reducer with no TopN

---
+++ ConditionalWordCount/src/test/java/Reducer1Test.java
@@ -0,0 +1,46 @@
+/**
+ * Testing on Reduce1 - Reducer with No TopN
+ */
+
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.*;
+import org.junit.Before;
+
+import chainMaps.Reduce1;
+
+
+public class Reducer1Test {
+
+	 // Fields
+	 // ------
+	 
+	 ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver;
+	 Reduce1 myReducer;
+	
+	
+	
+	  @Before
+	  public void setUp(){
+		  myReducer = new Reduce1();
+		  reduceDriver = new ReduceDriver<Text, IntWritable, Text, IntWritable>();
+		  reduceDriver.setReducer(myReducer);
+	  }
+	
+	
+	
+	  @Test
+	  public void testReducer1() throws IOException{
+	    List<IntWritable> values = new ArrayList<IntWritable>();
+	    values.add(new IntWritable(1));  
+	    values.add(new IntWritable(1));   // so values = {1,1}
+	    reduceDriver.withInput(new Text("cat"), values);
+	    reduceDriver.withOutput(new Text("cat"), new IntWritable(2));
+	    reduceDriver.runTest();
+	  }
+	 
+	  
+}
