53fd01a6f3045a1675d0b881e716e6fd29e4d697
hduser <github@newnham.com>
2015-09-19 12:35:14 +0100

    MeanTeamTester - Draft test for MeanMapper

--- Mean/src/test/java/mean/MeanTeamTester.java
+++ Mean/src/test/java/mean/MeanTeamTester.java
@@ -18,13 +18,13 @@ public class MeanTeamTester {
 	
 	// Fields
 		// ------
-		MapDriver<Object, Text, Text, IntWritable> mapDriver;
+		MapDriver<Object, Text, Text, MyCompoundValue> mapDriver;
 		MeanMapper myMapper;
 		
 		
 		@Before
 		public void setUp(){
-			mapDriver = new MapDriver<Object, Text, Text, IntWritable>();
+			mapDriver = new MapDriver<Object, Text, Text, MyCompoundValue>();
 			myMapper = new MeanMapper();
 			mapDriver.setMapper(myMapper);
 		}
@@ -39,19 +39,28 @@ public class MeanTeamTester {
 		public void testMap1() throws IOException, InterruptedException {
 		 Text value = new Text("rain spain rain");
 		                     
-		   mapDriver.withInput(new Text("Stewart"), new IntWritable(2));
-		   mapDriver.withInput(new Text("Stewart"), new IntWritable(3));
-		   mapDriver.withInput(new Text("Stewart"), new IntWritable(3));
-		   mapDriver.withInput(new Text("Mike"), new IntWritable(4));
-		   mapDriver.withInput(new Text("Mike"), new IntWritable(5));
-		   mapDriver.withInput(new Text("Mike"), new IntWritable(6));
-		   mapDriver.withInput(new Text("Rob"), new IntWritable(7));
-		   mapDriver.withInput(new Text("Rob"), new IntWritable(8));
-		   mapDriver.withInput(new Text("Rob"), new IntWritable(9));
+		   mapDriver.withInput(new LongWritable(1),new Text("Stewart	2"));
+		   mapDriver.withInput(new LongWritable(1), new Text("Stewart	3"));
+		   mapDriver.withInput(new LongWritable(1), new Text("Stewart	4"));
+		   mapDriver.withInput(new LongWritable(1), new Text("Mike	4"));
+		   mapDriver.withInput(new LongWritable(1), new Text("Mike	5"));
+		   mapDriver.withInput(new LongWritable(1), new Text("Mike	6"));
+		   mapDriver.withInput(new LongWritable(1), new Text("Rob	7"));
+		   mapDriver.withInput(new LongWritable(1), new Text("Rob	8"));
+		   mapDriver.withInput(new LongWritable(1), new Text("Rob	9"));
 		   
-		   mapDriver.withOutput(new Text("Mike"),new IntWritable(5));
-		   mapDriver.withOutput(new Text("Rob"),new IntWritable(8));
-		   mapDriver.withOutput(new Text("Stewart"), new IntWritable(2));
+		   
+		   mapDriver.withOutput(new Text("Stewart"),new MyCompoundValue(2,1));
+		   mapDriver.withOutput(new Text("Stewart"),new MyCompoundValue(3,1));
+		   mapDriver.withOutput(new Text("Stewart"),new MyCompoundValue(4,1));
+		   mapDriver.withOutput(new Text("Mike"),new MyCompoundValue(4,1));
+		   mapDriver.withOutput(new Text("Mike"),new MyCompoundValue(5,1));
+		   mapDriver.withOutput(new Text("Mike"),new MyCompoundValue(6,1));
+		   mapDriver.withOutput(new Text("Rob"),new MyCompoundValue(7,1));
+		   mapDriver.withOutput(new Text("Rob"),new MyCompoundValue(8,1));
+		   mapDriver.withOutput(new Text("Rob"),new MyCompoundValue(9,1));
+		  
+		
 		   mapDriver.runTest();
 		}
 }
