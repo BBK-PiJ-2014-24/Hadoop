5760f585b05341114ce42e45501c6789b5485c45
hduser <github@newnham.com>
2015-09-19 09:59:27 +0100

    MyCompoundKey - First Try of My Own Writable Implementation.

---
+++ Mean/src/main/java/mean/MyCompoundKey.java
@@ -0,0 +1,24 @@
+package mean;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+public class MyCompoundKey implements Writable{
+
+	
+	
+	
+	public void write(DataOutput out) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readFields(DataInput in) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
