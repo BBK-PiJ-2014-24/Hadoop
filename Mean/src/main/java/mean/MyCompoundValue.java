4b1a68a67b5f8f1024796f4a5dfbb67556bef455
hduser <github@newnham.com>
2015-09-19 11:11:40 +0100

    MyCompoundValue - Completed

--- Mean/src/main/java/mean/MyCompoundValue.java
+++ Mean/src/main/java/mean/MyCompoundValue.java
@@ -1,7 +1,9 @@
 /**
  * A Writable is used for making your own Compund Value Objects. Just
  * a container for transporter for carrying multiple variables in the
- * MR.
+ * MR. MyCompoundKey as it will serve as a compound value. As such, it 
+ * implements Writable. Nb. A CompoundKey would need to implement 
+ * WritableComparable (as the keys will need to be compared.)
  */
 
 package mean;
@@ -15,8 +17,20 @@ import org.apache.hadoop.io.WritableComparable;
 
 public class MyCompoundValue implements Writable{
 
+	// Fields
+	// ------
+	
 	private int total;
 	private int count;
+	
+	// Constructor
+	// -----------
+	
+	private MyCompoundValue(){
+		total = 0;
+		count = 0;
+	}
+	
 	
 	public MyCompoundValue(int total, int count){
 		this.total = total;
@@ -24,18 +38,30 @@ public class MyCompoundValue implements Writable{
 	}
 	
 
+	// Serializes the Data
+	// -------------------
 	public void write(DataOutput out) throws IOException {
 		out.writeInt(total);
 		out.writeInt(count);
 	}
-
+	
+	
+	// Deserializes the Data
+	// ---------------------
 	public void readFields(DataInput in) throws IOException {
 		total = in.readInt();
 		count = in.readInt();
 	}
 
 	
-	
+	// Static Way to Call a Constructor
+	// --------------------------------
+	// readFields(in) Will set the Fields
+	public static MyCompoundValue read(DataInput in) throws IOException{
+		MyCompoundValue cv = new MyCompoundValue();
+		cv.readFields(in);
+		return cv;
+	}
 	
 	
 	
