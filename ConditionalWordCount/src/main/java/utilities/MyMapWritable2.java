b8b45ce0085c100b56ef7505d0cc59f1dc974191
hduser <github@newnham.com>
2015-09-23 14:07:18 +0100

    MyMapWritable2 - Proper Inherited Child of MapWritable

---
+++ ConditionalWordCount/src/main/java/utilities/MyMapWritable2.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import java.util.Set;
+
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Writable;
+
+public class MyMapWritable2 extends MapWritable {
+
+	
+	public MyMapWritable2(){
+		super();
+	}
+	
+	@Override
+	public String toString(){
+		
+		String s = "";
+		
+		Set<Writable> keys = keySet();
+		for(Writable key : keys){
+			s+= key.toString() + "\t" + get(key).toString() + "\n";
+		}
+		return s;
+	}
+}
