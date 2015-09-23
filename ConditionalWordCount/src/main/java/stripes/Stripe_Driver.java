a10f7b2b5626cc81a2b62c99447843d7647df42d
hduser <github@newnham.com>
2015-09-23 15:42:54 +0100

    Stripe Driver - Refactored for MyMapWritable2

--- ConditionalWordCount/src/main/java/stripes/Stripe_Driver.java
+++ ConditionalWordCount/src/main/java/stripes/Stripe_Driver.java
@@ -10,6 +10,11 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import utilities.MyMapWritable;
+import utilities.MyMapWritable2;
 
 
 
@@ -17,15 +22,19 @@ public class Stripe_Driver {
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException{
 		
 		Configuration conf = new Configuration();
-	    Job job = Job.getInstance(conf, "pair word count");
+	    Job job = Job.getInstance(conf, "stripe word count");
 	    job.setJarByClass(Stripe_Driver.class);
 	    
 	    job.setMapperClass(Stripe_Mapper.class);
 	    job.setCombinerClass(Stripe_Reducer.class);
 	    job.setReducerClass(Stripe_Reducer.class);
 	    
+	 //   job.setMapOutputKeyClass(Text.class);
+	 //   job.setMapOutputValueClass(MyMapWritable2.class);
 	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(MapWritable.class);
+	    job.setOutputValueClass(MyMapWritable2.class);
+	    
+	    //job.setOutputFormatClass(TextOutputFormat.class);
 	    
 	    FileInputFormat.addInputPath(job, new Path(args[0]));
 	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
