2c58a506d67fcf6cdd30ea374e144e72a8405de8
hduser <github@newnham.com>
2015-09-23 16:44:49 +0100

    Stripe_Reducer - Reducer for Absolute Count (Not Relative Frequency)

--- ConditionalWordCount/src/main/java/stripes/Stripe_Reducer.java
+++ ConditionalWordCount/src/main/java/stripes/Stripe_Reducer.java
@@ -1,3 +1,6 @@
+/**
+ * Reducer for Absolute Count (Not Relative Frequency)
+ */
 package stripes;
 
 import java.io.IOException;
@@ -9,27 +12,37 @@ import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class Stripe_Reducer extends Reducer<Text, MapWritable, Text, MapWritable>{
+import utilities.MyMapWritable;
+import utilities.MyMapWritable2;
+
+public class Stripe_Reducer extends Reducer<Text, MyMapWritable2, Text, MyMapWritable2>{
 
 	
 	
-	private MapWritable result = new MapWritable();
+	private MyMapWritable2 result = new MyMapWritable2();
+
 	
-	public void reduce(Text key, Iterable<MapWritable> value, Context context) throws IOException, InterruptedException{
+	public void reduce(Text key, Iterable<MyMapWritable2> value, Context context) throws IOException, InterruptedException{
 		
 	result.clear();
 		
-		for(MapWritable val : value){
+		for(MyMapWritable2 val : value){
 			sumAll(val);
 		}
-		context.write(key, result);	
+		
+			//Text t = new Text(result.toString());
+			//System.out.println("\nHELLO\n");
+			//System.out.println(result.toString());
+			context.write(key, result); //result;	
 	}
 	
 	
-	private void sumAll(MapWritable mw){
+	private void sumAll(MyMapWritable2 mw){
 		Set<Writable> keys = mw.keySet();  // get the set of keys so u can iterate MapW
+										   // in actual fact, there is only 1 key in Set = for
+		
 		for(Writable key : keys){
-			
+		
 			IntWritable moreCount = (IntWritable) mw.get(key); // get the new count update
 			
 			if(result.containsKey(key)){   //if results map already has key
