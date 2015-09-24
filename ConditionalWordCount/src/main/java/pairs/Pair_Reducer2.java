54224cd6e0112a2ac5805a8e06a49cb087f36e6c
hduser <github@newnham.com>
2015-09-24 23:04:55 +0100

    PairReducer - Major Refactoring to allow for WordPair as input key and output key.

--- ConditionalWordCount/src/main/java/pairs/Pair_Reducer2.java
+++ ConditionalWordCount/src/main/java/pairs/Pair_Reducer2.java
@@ -5,24 +5,52 @@
 package pairs;
 
 import java.io.IOException;
+import java.util.Set;
 
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class Pair_Reducer2 extends Reducer<Text, IntWritable, Text, IntWritable> {
+import utilities.WordPair;
 
+public class Pair_Reducer2 extends Reducer<WordPair, IntWritable, WordPair, DoubleWritable> {
+
+	private Text star = new Text("*");
 	private IntWritable result = new IntWritable();
+	private DoubleWritable marginalCount = new DoubleWritable();
+	private DoubleWritable freq = new DoubleWritable();
 	
-	public void reduce(Text key, Iterable<IntWritable> value, Context context) throws IOException, InterruptedException{
 	
-		int sum = 0;
+	// key is ordered with (for,*) on top, because of the comparable
+	public void reduce(WordPair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+	
 		
-		for(IntWritable val : value){
-			sum += val.get();
+		// find the (for,*) first to calc marginalCount FIRST 
+		if(key.getRight().equals(star)){
+			marginalCount.set(marginalCount.get() + calcSum(values));
 		}
-		result.set(sum);
-		context.write(key, result);
+		else{
+			int count = calcSum(values);
+			freq.set((count*1.0)/marginalCount.get());
+			context.write(key, freq);
+		}
+	
 	}
+	
+	
+	// Method to calc any key's Partial sums by iterating its values
+	public int calcSum(Iterable<IntWritable> values){
+		int marg = 0;
+		for(IntWritable i : values){
+			marg += i.get();
+		}
+		return marg;
+	}
+	
+	
+	
+	
 }
