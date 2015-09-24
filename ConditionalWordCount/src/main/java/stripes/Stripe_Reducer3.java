2e42faf99ecbc1cdd179e78a82c02e59adf6af9e
hduser <github@newnham.com>
2015-09-24 15:20:54 +0100

    Stripe_Reduce3 - Reduce that calcs cond prob AND the topN!!!!

--- ConditionalWordCount/src/main/java/stripes/Stripe_Reducer3.java
+++ ConditionalWordCount/src/main/java/stripes/Stripe_Reducer3.java
@@ -7,6 +7,8 @@
 package stripes;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Collections;
 import java.util.Set;
 import java.util.TreeMap;
@@ -20,14 +22,15 @@ import org.apache.hadoop.mapreduce.Reducer.Context;
 
 import utilities.MyMapWritable2;
 
-public class Stripe_Reducer3 extends Reducer<Text, MyMapWritable2, Text, MyMapWritable2>{
+public class Stripe_Reducer3 extends Reducer<Text, MyMapWritable2, Text, DoubleWritable>{
 
 	// fields
 	// ------
 	
 	private MyMapWritable2 result = new MyMapWritable2();
+	private MyMapWritable2 topResult = new MyMapWritable2();
 	private IntWritable marginalCount = new IntWritable(0);
-	private TreeMap<IntWritable, Text> rankTree = new TreeMap<IntWritable, Text>(Collections.reverseOrder());
+	private TreeMap<Integer, Text> rankTree = new TreeMap<Integer, Text>(Collections.reverseOrder());
 	
 	
 	// Reduce
@@ -36,21 +39,35 @@ public class Stripe_Reducer3 extends Reducer<Text, MyMapWritable2, Text, MyMapWr
 		
 	result.clear();
 		
+	// Sum Stripes
+	// -----------
 		for(MyMapWritable2 stripe : value){
-			sumAll(stripe);
+			sumAll(stripe); // stripe (co-word, count) Amends Field Variable, result
 		}
+	// Select only Top 3 Scores
+	// ------------------------	
+		Set<Writable> sumKeys = result.keySet();
+		for(Writable sumKey : sumKeys){
+			Text coWord = (Text) sumKey;
+			int x  = ((IntWritable)(result.get(sumKey))).get();
+			
+			rankTree.put(x, coWord);
+			if (rankTree.size()>3)
+	    		rankTree.remove(rankTree.lastKey());    // push out the lowest count
+		}	
 	}
 	
-	
+	// MEthod that Sums Stripes from diff MAps
+	// ---------------------------------------
 	private void sumAll(MyMapWritable2 stripe){
-		Set<Writable> keys = stripe.keySet();  // get the set of keys so u can iterate MapW
-										   // in actual fact, there is only 1 key in Set = for
+		Set<Writable> keys = stripe.keySet();  // get the set of keys (of co-words) 
+										       // so u can iterate stripe
 		
-		for(Writable key : keys){
+		for(Writable key : keys){  // for each co-word
 		
 			IntWritable moreCount = (IntWritable) stripe.get(key); // get the new count update
 			
-			if(result.containsKey(key)){   //if results map already has key
+			if(result.containsKey(key)){   //if results map already has co-word in key
 				IntWritable count = (IntWritable)result.get(key); // get exist count
 				count.set(count.get() + moreCount.get());  // combine the two together.
 			}
@@ -61,23 +78,35 @@ public class Stripe_Reducer3 extends Reducer<Text, MyMapWritable2, Text, MyMapWr
 		}  // end for
 	}  // end sumAll
 	
+	
+	
+	// Final Reduce to Calc Cond Prob of Top 3
+	// ----------------------------------------
+	
 	@Override
 	protected void cleanup(Context context) throws IOException, InterruptedException{
 		
 		Set<Writable> keys = result.keySet();
+		//MyMapWritable2 prob = new MyMapWritable2();
 		
 		
-		for(Writable key : keys){
-			IntWritable finalCount = (IntWritable) result.get(key);
+		for(Integer i : rankTree.keySet()){
 			DoubleWritable freq = new DoubleWritable(500); // must inside loop so new freq for each line.
-			Double x =  (finalCount.get() *1.0)/marginalCount.get();
+			double x =  (i *1.0)/marginalCount.get();
+			x = round(x,4);
 			freq.set(x);
-			//System.out.println("******* " + finalCount + "/" + marginalCount  + "=" + freq);
-			//System.out.println("++++++++ " + key + " " + freq);
-			result.put(key, freq);
-			// System.out.println("++++++++ " + result.toString());
+			Text coWord = rankTree.get(i);
+			context.write(coWord, freq);
 		}
-		context.write(new Text("for"), result);
 	}
+	
+	private double round(double d, int dp){
+		
+		BigDecimal bd = new BigDecimal(d);
+		bd = bd.setScale(dp, RoundingMode.HALF_UP);
+		return bd.doubleValue();
+	}
+	
+	
 	
 }
