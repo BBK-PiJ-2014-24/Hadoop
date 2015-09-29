916262ddcef3fa1c461f3dff15f721c35d4976dc
hduser <github@newnham.com>
2015-09-29 13:09:45 +0100

    IntWritableArray - Added toString()

--- PageRank/src/main/java/utilities/IntWritableArray.java
+++ PageRank/src/main/java/utilities/IntWritableArray.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import java.io.DataOutput;
+import java.io.IOException;
+
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 
@@ -8,6 +11,32 @@ public class IntWritableArray extends ArrayWritable{
 	
 	public IntWritableArray(){
 		super(IntWritable.class); // Constructor is passed class of Writable
+	}
+	
+	@Override
+	public IntWritable[] get(){
+		return (IntWritable[]) super.get();
+	}
+	
+/*	
+	@Override
+	public void write(DataOutput out) throws IOException{
+		for(IntWritable i : get()){
+			i.write(out);
+		}
+	}
+*/
+	
+	
+	@Override
+	public String toString(){
+		
+		String s = "";
+		IntWritable[] values = get();
+		for(IntWritable i : values){
+			s = Integer.toString(i.get());
+		}	
+		return s;
 	}
 	
 	
