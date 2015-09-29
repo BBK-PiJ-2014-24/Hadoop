dbd33b7ea0acedd1f1d5ce9a6f95c068270347fa
hduser <github@newnham.com>
2015-09-29 14:45:25 +0100

    IntArrayTests -  Tested Array length and elements.

--- PageRank/src/test/java/UtilityTest/IntArrayTests.java
+++ PageRank/src/test/java/UtilityTest/IntArrayTests.java
@@ -4,11 +4,17 @@
 
 package UtilityTest;
 
+
+
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Writable;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
 
 import utilities.IntWritableArray;
 
@@ -16,27 +22,47 @@ public class IntArrayTests {
 
 	
 	private IntWritableArray intArr;
+	private IntWritable[] arr;
 	private ArrayWritable wa;
 	
 	@Before
 	public void setUp(){
 		intArr = new IntWritableArray();
 		
-		IntWritable[] arr = new IntWritable[3];
+		arr = new IntWritable[3];
 		arr[0] = new IntWritable(10);
 		arr[1] = new IntWritable(11);
 		arr[2] = new IntWritable(12);
 		
 		
-		intArr.put(new IntWritable(10));
-		intArr.set(new IntWritable(11));
-		intArr.set(new IntWritable(12));
+		intArr.set(arr);
+		
 	}
 	
+	/**
+	 * Test length of Array
+	 */
 	@Test
 	public void test1(){
+		System.out.println("size: " + intArr.length());
+
+		assertEquals("Check Array length: ", intArr.length(), 3);
 		
-		
+	}
+	
+	/**
+	 * Test Elements 
+	 */
+	@Test
+	public void test2(){
+		IntWritable[] innerArray = intArr.get();
+	
+		int k=0;
+		for(IntWritable i : innerArray){
+			assertTrue(Arrays.asList(innerArray).contains(arr[k]));
+			System.out.println(arr[k]);
+			k++;
+		}
 	}
 	
 	
