898096f7699dd0eacfaa98977fa10525da33de68
hduser <github@newnham.com>
2015-09-29 15:59:35 +0100

    Mapper_Basic - Added Mapper_Basic

---
+++ PageRank/src/main/java/basic/Mapper_Basic.java
@@ -0,0 +1,11 @@
+package basic;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import utilities.Node;
+
+public class Mapper_Basic extends Mapper<Object,IntWritable, IntWritable, Node >{
+	
+	
+
+}
