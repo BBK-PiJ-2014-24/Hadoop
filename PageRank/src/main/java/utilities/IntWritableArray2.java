c27ccda64f8b3fda03924b46f2a20fdd9541c6cb
hduser <github@newnham.com>
2015-09-30 16:09:55 +0100

    IntWritableArray2 - Class Now Extending ArrayPrimitiveWritable

---
+++ PageRank/src/main/java/utilities/IntWritableArray2.java
@@ -0,0 +1,72 @@
+package utilities;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.ArrayPrimitiveWritable;
+
+public class IntWritableArray2  extends ArrayPrimitiveWritable {
+
+	private int[] arr;
+	
+	public IntWritableArray2(int size){
+		this.arr = new int[size];
+	}
+	
+	public IntWritableArray2(int[] arr){
+		this.arr = arr;
+	}
+	
+	public int[] get(){
+		return this.arr;
+	}
+	
+	public void set(int[] arr){
+		this.arr = arr;
+	}
+	
+	public int lenth(){
+		return arr.length;
+	}
+	
+	
+	public void write(DataOutput out) throws IOException{
+		for(Integer i : arr){
+			out.writeInt(i);
+		}
+	}
+	
+	
+	public void readin(DataInput in) throws IOException{
+		this.arr = new int[100];
+		int k=0;
+		for(Integer i : arr){
+			arr[k] = i;
+		}
+	}
+	
+	
+	@Override
+	public String toString(){
+		
+		String s = "";
+		int size = arr.length;
+		int k=0;
+		for(Integer i : arr){
+			if (k == size -1)
+				s += Integer.toString(i);
+			else
+				s+= Integer.toString(i) + ", ";
+
+			k++;
+		}
+		return s;		
+	}
+
+
+	
+	
+	
+	
+}
