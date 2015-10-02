75d297dbb8b764954eeaaa0644c1d8a250418591
hduser <github@newnham.com>
2015-10-02 19:10:32 +0100

    IntArrayTests2 - Added compareTo(). Seems to resolve the Serialization/ Deserialization issue

--- PageRank/src/main/java/utilities/IntWritableArray2.java
+++ PageRank/src/main/java/utilities/IntWritableArray2.java
@@ -5,11 +5,16 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 import org.apache.hadoop.io.ArrayPrimitiveWritable;
+import org.apache.hadoop.io.IntWritable;
 
 public class IntWritableArray2  extends ArrayPrimitiveWritable {
 
 	private int[] arr;
 	private int size;
+	
+	public IntWritableArray2(){
+		super();
+	}
 	
 	public IntWritableArray2(int size){
 		this.arr = new int[size];
@@ -37,22 +42,38 @@ public class IntWritableArray2  extends ArrayPrimitiveWritable {
 	
 	public void write(DataOutput out) throws IOException{
 		
-		out.writeInt(this.size);
-		for(int i=0;i<size;i++){
-			out.writeInt(this.arr[i]);
-		}
+		
+	this.write(out);	
+	//	out.writeInt(this.size);
+	//	for(int i=0;i<size;i++){
+	//		out.writeInt(this.arr[i]);
+	//	}
 	//	this.write(out);
 	}
 	
 	
-	public void readin(DataInput in) throws IOException{
+	public void readfields(DataInput in) throws IOException{
 		
-		size = in.readInt();
-		this.arr = new int[size];
-		for(int i=0; i<size; i++){
-			this.arr[i] = in.readInt();
-		} 
+	this.readfields(in);	
+		//adjListArray.write
+		
+	//	size = in.readInt();
+	//	this.arr = new int[size];
+	//	for(int i=0; i<size; i++){
+	//		this.arr[i] = in.readInt();
+	//	} 
 	}
+	
+	
+	public int compareTo(Object o){
+		
+		IntWritableArray2 other = (IntWritableArray2) o;
+		//return this.size > other.length() ? 1 : 0;
+		//this.size > other.length();
+			return this.size - other.length();
+	
+	}
+	
 	
 	
 	@Override
